738aa64dbf11ec3a61593f1d3e8a22bc9e7b8a1d
Heeje <tjdgmlwp34567@gmail.com>
2023-10-23 00:46:52 +0900

    [Refactoring] Module화

--- refactoring3/app.js
+++ refactoring3/app.js
@@ -1,8 +1,6 @@
-/**
- * 계산 단계와 포맷팅 단계 분리
- */
+import createStatementData from "./createStatementData.mjs";
+import fs from 'fs';
 
-const fs = require('fs');
 
 function readJSONFile(filePath) {
     try {
@@ -15,83 +13,8 @@ function readJSONFile(filePath) {
 }
 
 function statement(invoice) {
-    const statementData = {}; // 중간 데이터 구조 객체 생성
-    statementData.customer = invoice[0].customer;
-    statementData.performances = invoice[0].performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlainText(statementData) // html 결과 출력을 함수로 추출
+    return renderPlainText(createStatementData(invoice))
 
-    function enrichPerformance(aPerformance){
-        const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-
-        return result;
-    }
-
-    function playFor(aPerformance){
-        return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance){
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case "tragedy": //비극
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-        }
-        return result;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-
-        let result = 0;
-
-        result += Math.max(aPerformance.audience - 30, 0);
-
-        if ("comedy" === aPerformance.play.type) {
-            result += Math.floor(aPerformance.audience / 5);
-        }
-
-        return result;
-    }
-
-    function totalAmount(data){
-        // for문 -> 파이프라인 처리
-        return data.performances
-            .reduce((total, p) => total + p.amount, 0);
-        /*
-        let result = 0;
-        for (let performance of data.performances) {
-            result += performance.amount;
-        }
-        return result;
-        */
-    }
-
-    function totalVolumeCredits(data){
-        return data.performances
-            .reduce((total,p) => total + p.volumeCredits, 0);
-        // let result = 0;
-        // for (let performance of data.performances) {
-        //     result += performance.volumeCredits;
-        // }
-        // return result;
-    }
 }
 
 function renderPlainText(data){
